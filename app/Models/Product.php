61cf1e1513e3e9839917cb5f34e0166da5f15353
Kolalexx <kolalexx@yandex.ru>
2025-06-13 22:01:11 +0300

    Add soft delete for products

--- app/Models/Product.php
+++ app/Models/Product.php
@@ -6,16 +6,19 @@ use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
 use Illuminate\Database\Eloquent\Relations\BelongsTo;
 use Illuminate\Database\Eloquent\Relations\HasMany;
+use Illuminate\Database\Eloquent\SoftDeletes;
 
 class Product extends Model
 {
     use HasFactory;
+    use SoftDeletes;
 
     protected $fillable = [
         'name',
         'category_id',
         'description',
         'price',
+        'deleted_at',
     ];
 
     public function category(): BelongsTo
